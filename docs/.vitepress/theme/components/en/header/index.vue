fa836b8dc0335bc45e24e792b9cd8eebc5d22922
yuanqiliang <yuanqiliang@uniontech.com>
2022-08-11 18:10:22 +0800

    fix: 修复英文url错误
    Log:

--- docs/.vitepress/theme/components/en/header/index.vue
+++ docs/.vitepress/theme/components/en/header/index.vue
@@ -6,7 +6,7 @@
       <div class="nav u-flex u-item-center u-justify-start">
         <a class="selected" href="/">Home</a>
         <a href="http://10.0.33.45:28803/" target="_blank">Linglong Store</a>
-        <a href="javascript:void(0);" @click="jump('/guide/start/install.html')">User Manual</a>
+        <a href="javascript:void(0);" @click="jump('/en/guide/start/install.html')">User Manual</a>
         <i class="active"></i>
       </div>
       <!-- 语言切换 -->
