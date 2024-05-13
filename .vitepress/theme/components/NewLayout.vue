4254dc3457b49df2a98da94e5c60a594d6c7f04b
bluesky <chenchongbiao@deepin.org>
2024-05-13 16:48:32 +0800

    feat: update sidebar
    * 玲珑文档新增，修改 sidebar
    * 修改获取版本号的方式,以及默认版本号
    * 删除获取1.4.3的工作流
    
    Log: update sidebar

--- .vitepress/theme/components/NewLayout.vue
+++ .vitepress/theme/components/NewLayout.vue
@@ -5,13 +5,23 @@ SPDX-License-Identifier: LGPL-3.0-or-later
 -->
 
 <template>
-  <Layout v-if="lay.isDefault" />
+  <Layout v-if="lay.isDefault">
+    <template #doc-before>
+      <div class="tip custom-block">
+        <p class="custom-block-title">TIP</p>
+        <p>{{tips}}</p>
+      </div>
+      <br/>
+    </template>
+  </Layout>
   <Home v-if="lay.isHome" />
   <EnHome v-if="lay.isEnHome" />
 </template>
 <script setup>
 import Home from './zh/home/index.vue'
 import EnHome from './en/home/index.vue'
+
+import { generateVersions } from '../versions.js';
 
 import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
@@ -35,6 +45,39 @@ const lay = computed(() => {
   }
   return _lay
 })
+
+const tips = computed(() => {
+  const { defaultVersion } = generateVersions()
+  let pathParts = route.path.split('/')
+  let versionPart = pathParts.find(part => /^\d+\.\d+\.\d+$/.test(part))
+  let _tips = ""
+
+  if (route.path.includes('/en/')) {
+    _tips = "This document is applicable to version "
+
+    if (versionPart === undefined) {
+      _tips = _tips + defaultVersion
+    } else {
+      _tips = _tips + versionPart
+    }
+
+    _tips = _tips + ", Please use the command ll-cli --version to check your LingLong program version."
+  } else {
+    _tips = "此文档适用于 "
+
+    if (versionPart === undefined) {
+      _tips = _tips + defaultVersion
+    } else {
+      _tips = _tips + versionPart
+    }
+
+    _tips = _tips + ", 请使用 ll-cli --version 检查您的玲珑程序版本。"
+  }
+
+  return _tips
+})
+
+// console.log(tips)
 
 // onMounted(() => {
 //   if (route.path === '/en/') {
--- .vitepress/theme/versions.js
+++ .vitepress/theme/versions.js
@@ -1,2 +1,7 @@
-const versions = [''];
-exports.versions = versions;
+export function generateVersions() {
+    const versions = ['']; // 保留空用来判断默认路由
+    return {
+      defaultVersion: '1.5.0',  // 默认显示版本号
+      versions: versions
+    };
+}
